5de3e112a891a96079037e590d4b9425433e4767
Chinmay Joshi <chinmaysjoshi@gmail.com>
2022-08-08 19:51:07 +0530

    1. ADD : some more new imports 2. ADD : created send to slack function and exposed it to anvil 3. ADD : add anvil server connect in init

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import ast
+import json
 from urllib import request
+from datetime import datetime
+import anvil.server
 from flask import Flask
 from apscheduler.schedulers.background import BackgroundScheduler as bkgSch
 
@@ -18,6 +21,32 @@ def hello_world():
 def init():
     global all_info_dict
     all_info_dict = ast.literal_eval(os.environ['all_info_dict'])
+    print(all_info_dict)
+    anvil.server.connect(all_info_dict['anvil_server_uplink_url'])
+
+
+@anvil.server.callable
+def send_to_slack(channel, message, host='REPLIT'):
+    if 'slack_uri' not in all_info_dict:
+        print(f'No slack uri hence cannot send message {channel} {message}')
+        return 1
+    slack_uri = all_info_dict['slack_uri']
+    message = message.replace(' ', ' ')
+    post = {"text": f"{message}", "channel": channel, "username": host,
+            "title": "Yo"}
+    try:
+        json_data = json.dumps(post)
+        req = request.Request(slack_uri, data=json_data.encode('ascii'),
+                              headers={'Content-Type': 'application/json'})
+        _ = request.urlopen(req)
+    except Exception as em:
+        prefix = f'\033[91m' + '{0:18}\033[00m : '.format('send_to_slack')
+        dt = datetime.now()
+        the_time = dt.strftime("%H:%M:%S.%f")
+        if str(em).find('400') == -1:
+            print(the_time + ' : ' + prefix + 'FAIL ' + str(em))
+        return False
+    return True
 
 
 sch_01.add_job(init, misfire_grace_time=120)
